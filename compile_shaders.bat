4bed96dfebcf04934a61fd2f642ca86dc4a42db1
Jesus Diaz <jdiaz@DLE.ent>
2020-10-05 17:54:12 +0200

    Repository cleaning

--- compile_shaders.bat
+++
@@ -1,2 +0,0 @@
-glslc main_shader.vert -o main_shader_vert.spv
-glslc main_shader.frag -o main_shader_frag.spv
--- debug.bat
+++
@@ -1,3 +0,0 @@
-REM Open the executable with the MS vistual studio devenv
-
-devenv /debugexe build\main.exe
